880e9eb769f167878a9ada864bfa57cf66c357f5
wangyang <1749748955@qq.com>
2024-03-31 19:57:58 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,7 +1,16 @@
+yarn build
+
+
 cd dist
-git init
-git remote add github git@github.com:bioproj/pipeline-proj-ui.git
+
+git clone -b main --single-branch git@github.com:bioproj/pipeline-proj-ui.git 
+cd pipeline-proj-ui
+git reset d737c5f5cddcc06939ff5b6e2d53366790a8ba79 --hard
+
+
+mv ../*  .
+
 git add .
 git commit -m 'update'
-git branch main
-git push -u github main -f
+
+git push -u origin main -f 
