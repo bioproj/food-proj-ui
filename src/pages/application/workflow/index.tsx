9c8c30575b7678b5640bf65820eaf8f00d71099c
wangyang <1749748955@qq.com>
2024-04-03 22:11:22 +0800

    update

--- src/pages/application/workflow/index.tsx
+++ src/pages/application/workflow/index.tsx
@@ -9,39 +9,40 @@ import MyButton from '@/components/basic/button';
 import MyPage from '@/components/business/page';
 
 const tableColums: MyPageTableOptions<Application> = [
-  {
-    title: 'Name',
-    children: [
-      { title: 'First Name', dataIndex: 'firstName', key: 'firstName' },
-      { title: 'Last Name', dataIndex: 'lastName', key: 'lastName' },
-    ],
-  },
-  { title: 'Age', dataIndex: 'age', key: 'age' },
-  { title: 'Address', dataIndex: 'address', key: 'address' },
-  {
-    title: 'Tags',
-    dataIndex: 'tags',
-    key: 'tags',
-    render: (tags, record) => (
-      <>
-        {record.tags.map(tag => (
-          <Tag color="blue" key={tag}>
-            {tag}
-          </Tag>
-        ))}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <MyButton type="text">Invite {record.lastName}</MyButton>
-        <MyButton type="text">Delete</MyButton>
-      </Space>
-    ),
-  },
+  // {
+  //   title: 'Name',
+  //   children: [
+  //     { title: 'First Name', dataIndex: 'firstName', key: 'firstName' },
+  //     { title: 'Last Name', dataIndex: 'lastName', key: 'lastName' },
+  //   ],
+  // },
+  { title: 'name', dataIndex: 'name', key: 'name' },
+  { title: 'pipeline', dataIndex: 'pipeline', key: 'pipeline' },
+  
+  // {
+  //   title: 'Tags',
+  //   dataIndex: 'tags',
+  //   key: 'tags',
+  //   render: (tags, record) => (
+  //     <>
+  //       {record.tags.map(tag => (
+  //         <Tag color="blue" key={tag}>
+  //           {tag}
+  //         </Tag>
+  //       ))}
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: 'Action',
+  //   key: 'action',
+  //   render: (_, record) => (
+  //     <Space size="middle">
+  //       <MyButton type="text">Invite {record.lastName}</MyButton>
+  //       <MyButton type="text">Delete</MyButton>
+  //     </Space>
+  //   ),
+  // },
 ];
 
 const BusinessBasicPage: FC = () => {
