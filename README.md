7f33f0ae60f6718d0ea753c1f3315b6ae7270472
wangyang <1749748955@qq.com>
2024-04-05 12:02:28 +0800

    update readme

--- README.md
+++ README.md
@@ -18,5 +18,36 @@
 + 🌐 项目预览: <http://yun.bioinfo.online>
 + 💡项目贡献: <https://bioinfo.online/articleList/20242274568.html>
 
+## 快速使用
+#### 第一步
+打开：<http://yun.bioinfo.online>
+> 注：为保证数据的安全，这里前端项目会访问本地localhost的api服务，因此需要第二步，在本的运行后端服务
+#### 第二步
+在本地运行以下命名，启动后端服务
+> 注：需要安装docker
+```
+  # 创建网络
+  docker network create pipeline
+  # 启动mongo数据库
+  docker run --rm -d \
+    --name mongo \
+    --network pipeline \
+    --hostname mongo \
+    -p 27018:27017 \
+    registry.cn-hangzhou.aliyuncs.com/wybioinfo/mongo
+  # 启动后端程序
+  docker run --rm \
+    --name service-pipeline \
+    --network pipeline \
+    -e MONGO_HOST=mongo \
+    -e MONGO_PORT=27017 \
+    -e MONGO_DB=pipeline_db \ 
+    -p 30001:30001 \
+    registry.cn-hangzhou.aliyuncs.com/wybioinfo/service-pipeline-boot-1.0.jar
+```
+
+
+
 ## 开发
 + 测试代理是否正常： <http://localhost:8889/pipeline/hello>
+
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react';
 
 import './index.less';
+import type { ColProps } from 'antd/es/col';
 
 import { useEffect, useState } from 'react';
 
@@ -8,7 +9,7 @@ import Overview from './overview';
 import SalePercent from './salePercent';
 import TimeLine from './timeLine';
 import { BASE_URL } from '@/config'
-import { Card } from 'antd';
+import { Card, Row, Col, Divider } from 'antd';
 
 
 const DashBoardPage: FC = () => {
@@ -41,13 +42,53 @@ function MyComponent() {
   const isDevelopment = import.meta.env.MODE === 'development';
   const isProduction = import.meta.env.MODE === 'production';
   console.log(import.meta.env)
-  return (
-    <div>
 
-      {isDevelopment && <p>开发环境</p>}
-      {isProduction && <p>生产环境</p>}
-      {BASE_URL}
-    </div>
+  const wrapperCol: ColProps = {
+    xs: 24,
+    sm: 24,
+    md: 12,
+    lg: 12,
+    xl: 12,
+    xxl: 12,
+  };
+  return (
+    <Row gutter={20} >
+      <Col {...wrapperCol} order={0} style={{ borderRight: '1px  solid rgba(5, 5, 5, 0.06)' }} >
+        {isDevelopment && <p>开发环境: {BASE_URL}</p>}
+        {isProduction && <p>生产环境: {BASE_URL}</p>}
+
+      </Col>
+
+      <Col {...wrapperCol} order={1} >
+        <p>使用docker快速开始</p>
+      
+        <pre>
+          <code>
+            {`
+  # 创建网络
+  docker network create pipeline
+  # 启动mongo数据库
+  docker run --rm -d \\
+    --name mongo \\
+    --network pipeline \\
+    --hostname mongo \\
+    -p 27018:27017 \\
+    registry.cn-hangzhou.aliyuncs.com/wybioinfo/mongo
+  # 启动后端程序
+  docker run --rm \\
+    --name service-pipeline \\
+    --network pipeline \\
+    -e MONGO_HOST=mongo \\
+    -e MONGO_PORT=27017 \\
+    -e MONGO_DB=pipeline_db \\ 
+    -p 30001:30001 \\
+    registry.cn-hangzhou.aliyuncs.com/wybioinfo/service-pipeline-boot-1.0.jar`
+            }
+          </code>
+        </pre>
+
+      </Col>
+    </Row>
   );
 }
 
